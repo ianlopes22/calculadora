f42757d470957d78a1d6c381c839b381930c0bb1
ianlopes22 <73292954+ianlopes22@users.noreply.github.com>
2020-10-22 14:18:32 -0300

    Primeiro commit

---
+++ angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+    return {heroes};
+  }
+
+  // Substitui o método genId para garantir que um herói sempre tenha um id.
+  // Se a array de heróis estiver vazia,
+  // o método abaixo retorna o número inicial (11).
+  // se a array de heroes não estiver vazia, o método abaixo retorna o mais alto
+  // id do herói + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
